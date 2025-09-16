025ef375e509c6c0786b25b9f1db2e93f7eed69f
mathusan0826 <jevamathu0826@gmail.com>
2025-09-16 16:48:18 +0530

    carimage

--- CarRental/update_images.sql
+++ CarRental/update_images.sql
@@ -6,5 +6,15 @@ UPDATE Vehicles SET ImageUrl = '/images/Toyota Hiace.jpeg' WHERE Name = 'Toyota 
 UPDATE Vehicles SET ImageUrl = '/images/Mitsubshi Delica.jpeg' WHERE Name = 'Mitsubishi Delica';
 UPDATE Vehicles SET ImageUrl = '/images/Toyota Coaster.jpeg' WHERE Name = 'Toyota Coaster';
 UPDATE Vehicles SET ImageUrl = '/images/Jeep Wrangler.jpeg' WHERE Name = 'Jeep Wrangler';
+UPDATE Vehicles SET Imagurl = '/images/Volvo.jpeg' WHERE Name = 'Volvo'
+UPDATE Vehicles SET Imagurl = '/images/Ford.jpeg' WHERE Name = 'Ford';
+UPDATE Vehicles SET Imagurl = '/images/Audi.jpeg' WHERE Name = 'Audi';
+UPDATE Vehicles SET Imagurl = '/images/Saab.jpeg' WHERE Name = 'Saab';
+UPDATE Vehicle SET Imagurl = '/images/Mini.jpeg' WHERE Name = 'Mini';
+UPDATE Vehicle SET Imagurl = '/images/Honda.jpeg' WHERE Name = 'Honda';
+UPDATE Vehicle SET Imagurl = '/images/NightShade.jpeg' WHERE Name = 'NightShade';
+UPDATE Vehicle SET Imagurl = '/images/Flamingo.jpeg' WHERE Name = 'Flamingo';
+
+
 
 //
