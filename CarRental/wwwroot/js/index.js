90f99c7471a5bf28c56ae43106fab445a155f2cf
mathusan0826 <jevamathu0826@gmail.com>
2025-09-23 07:33:11 +0530

    jsfiles & css

---
+++ CarRental/wwwroot/js/index.js
@@ -0,0 +1,16 @@
+﻿//<script>
+//    function searchVehicles() {
+//        const type = document.getElementById('vehicleType').value;
+//    const seats = document.getElementById('seats').value;
+//    const pickupDate = document.getElementById('pickupDate').value;
+//    const rentalDays = document.getElementById('rentalDays').value;
+
+//    let url = '/Vehicle?';
+//    if (type) url += `type=${type}&`;
+//    if (seats) url += `seats=${seats}&`;
+//    if (pickupDate) url += `pickupDate=${pickupDate}&`;
+//    if (rentalDays) url += `rentalDays=${rentalDays}&`;
+
+//    window.location.href = url;
+//    }
+//</script>
